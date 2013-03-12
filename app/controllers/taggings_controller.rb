08bd37e7d3621ce8befa96d54a3fda1b501e51c7
Duyet Nguyen <duyetln@yahoo.com>
2013-03-12 01:18:15 -0700

    Added interface for Tagging

--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -3,16 +3,18 @@ class TaggingsController < ApplicationController
   before_filter :require_admin_user
 
   def create
-    @tagging = Tagging.new
-    @tagging.tag_id = params[:tag_id]
-    @tagging.taggable_id = params[:taggable_id]
-    @tagging.taggable_type = params[:taggable_type]
-      
-    if @tagging.save
-      redirect_to :back
-    else
-      redirect_to :back
+    
+    Tagging.delete_all(["taggable_type = ? AND taggable_id = ?", params[:taggable_type], params[:taggable_id]])
+    
+    params[:tag_ids].each do |id|
+      tagging = Tagging.new
+      tagging.tag_id = id
+      tagging.taggable_id = params[:taggable_id]
+      tagging.taggable_type = params[:taggable_type]
+      tagging.save
     end
+      
+    redirect_to :back
   end
   
   def destroy
